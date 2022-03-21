02ec8f80b5de1b31ff62012c3d6ccdbf75c9ee8a
Marjone <marjone@maddlogic.com>
2022-03-21 09:29:09 +0800

    Added route function

--- src/index.js
+++ src/index.js
@@ -3,41 +3,52 @@ const MLPermissions = {
   install(app, options = {}) {
 
     app.directive('can', (el, binding, vnode) => {
-        const { user } = options;
+      const { user } = options;
 
-        el.style.visibility = "hidden";
+      el.style.visibility = "hidden";
 
-        const permissions = user.permissions.map(p => { return p.toLowerCase(); });
+      const permissions = user.permissions.map(p => { return p.toLowerCase(); });
 
-        let ok = permissions.includes(`${binding.arg} ${binding.expression}`);
+      let ok = permissions.includes(`${binding.arg} ${binding.expression}`);
 
-        const show = binding.modifiers.show;
+      const show = binding.modifiers.show;
 
-        console.log(permissions, options, ok, show);
+      console.log(permissions, options, ok, show);
 
-        vnode.context.$nextTick(() => {
-          if(show){
-            if (!ok) {
+      vnode.context.$nextTick(() => {
+        if(show){
+          if (!ok) {
+            vnode.elm.parentElement.removeChild(vnode.elm);
+          }
+        }else{
+          const behavior = binding.modifiers.disable ? 'disable' : 'hide';
+          if (!ok) {
+            if (behavior === 'hide') {
               vnode.elm.parentElement.removeChild(vnode.elm);
-            }
-          }else{
-            const behavior = binding.modifiers.disable ? 'disable' : 'hide';
-            if (!ok) {
-              if (behavior === 'hide') {
-                vnode.elm.parentElement.removeChild(vnode.elm);
-              } else if (behavior === 'disable') {
-                el.disabled = true;
-                el.style.visibility = ""
-              }
+            } else if (behavior === 'disable') {
+              el.disabled = true;
+              el.style.visibility = ""
             }
           }
-        });
-
-        if(ok){
-          el.style.visibility = ""
         }
-
       });
+
+      if(ok){
+        el.style.visibility = ""
+      }
+
+    });
+
+    //params i.e segment = ["tasks", "add"]
+    app.isPermittedRoute = function(segment) {
+      const permissions = user.permissions.map(p => { return p.toLowerCase(); });
+
+      const arg = segment[1];
+      const exp = segment[0];
+
+      return permissions.includes(`${arg} ${exp}`);
+
+    };
   }
 };
 
